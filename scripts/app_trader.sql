eb5a27288f6bc253f33df8502f1ea815be4ded58
Sobia005 <nskousar123@gmail.com>
2024-10-19 12:44:08 -0500

    finished work

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -9,41 +9,6 @@ ORDER BY name;
 
 
 
-SELECT    DISTINCT ON (name) name
-      ,     MAX(PRICE) as price
-      ,     COUNT(name)*5000 AS profit_per_month
-      ,     COUNT(DISTINCT name)*1000 AS cost_per_month
-      ,     CAST((ROUND(AVG(rating)*2.0)/2.0)*2 +1 AS DECIMAL(5,2)) AS expected_life
-      ,     CASE WHEN COUNT(name) > 1 THEN 'Y' ELSE 'N' END AS available_in_both_stores
-      ,     MAX(genre) AS genre
-FROM
-(
-SELECT      DISTINCT ON (name) name
-      ,     MAX(CASE WHEN price = '0.00' THEN 10000 else CEILING(price)*10000 END) AS Price
-      ,     AVG(rating) AS rating
-      ,     MAX(primary_genre) AS genre
-FROM app_store_apps
-GROUP BY name
-UNION ALL
-SELECT      DISTINCT ON (name) name
-      ,     MAX(CASE WHEN CAST(REPLACE(price, '$', '')AS NUMERIC) = 0 THEN 10000 
-                  ELSE CEILING(CAST(REPLACE(price, '$','')AS NUMERIC))*10000 END) AS price
-      ,     AVG(rating)
-      ,     MAX(genres)
-FROM play_store_apps
-GROUP BY name)
-GROUP BY name;
-
-
-    SELECT app.name, 
-    app.price as app_price,
-    play.price as play_price, 
-    app.rating as app_rating, 
-    play.rating as play_rating
-    FROM app_store_apps as app
-    LEFT JOIN play_store_apps as play
-    ON app.name = play.name;
-
 
     SELECT *
 	FROM app_store_apps
@@ -51,4 +16,87 @@ GROUP BY name;
 
 	SELECT *
 	FROM play_store_apps
-	
+
+
+
+   SELECT DISTINCT(name),
+   MAX(price) as price,
+   COUNT(name)*5000 as profitPerMonth,
+   COUNT(DISTINCT name)*1000 as costPerMonth,
+   CAST((ROUND(AVG(rating)*2.0)/2.0)*2 +1 as decimal(5,2)) as avg_rating
+   ,CASE WHEN COUNT(name) > 1 THEN 'y' ELSE 'n' END AS availableInBothStores
+   
+   FROM
+   (
+   SELECT DISTINCT(name),
+   CASE WHEN price = '0.00' THEN 10000 ELSE CEILING(price)*10000 END AS price,rating
+  
+   FROM app_store_apps
+   UNION
+  SELECT DISTINCT(name),
+  CASE WHEN CAST(REPLACE(price,'$','') as numeric) = 0 THEN 10000 ELSE
+  CEILING(CAST(REPLACE(price,'$','') as numeric))*10000 END AS price, rating
+  
+  FROM play_store_apps)
+  GROUP BY name;
+
+
+
+
+
+
+
+
+	WITH play_store_corr AS (
+	SELECT name, REPLACE(REPLACE(play_store_apps.price,'$',''),' ','')::numeric AS price_trimmed, rating, review_count
+	FROM play_store_apps
+),
+prices_and_ratings AS (
+	SELECT name, price, rating, review_count::integer
+	FROM app_store_apps
+	UNION
+	SELECT name, price_trimmed AS price, rating, review_count
+	FROM play_store_corr
+	ORDER BY name
+ ),
+cost_and_lifetime AS(
+	SELECT name
+		,	CASE WHEN MAX(price) < 1 THEN 10000
+			ELSE MAX(price)*10000 END AS purchase_cost
+		,	CASE WHEN ROUND(AVG(rating)*2)+1 IS NULL then 1
+			ELSE ROUND(AVG(rating)*2)+1 END AS lifetime
+		,	SUM(review_count) as total_reviews
+	FROM prices_and_ratings
+	GROUP BY name
+	ORDER BY purchase_cost DESC
+),
+annual_revenue AS(
+	SELECT name
+		,	CASE WHEN app.price IS NOT NULL AND play.price_trimmed IS NOT NULL THEN 108000
+				WHEN app.price IS NULL OR play.price_trimmed IS NULL THEN 48000
+				END as annual_revenue
+	FROM app_store_apps AS app
+		FULL OUTER JOIN play_store_corr AS play
+			USING (name)
+	ORDER BY name
+),
+total_revenue AS(
+SELECT DISTINCT name
+	,	ann.annual_revenue
+	,	cost.lifetime
+	,	cost.purchase_cost
+	,	cost.total_reviews
+	,	(ann.annual_revenue*cost.lifetime)-cost.purchase_cost as total_revenue
+FROM annual_revenue AS ann
+	FULL OUTER JOIN cost_and_lifetime AS cost
+		USING (name)
+ORDER BY total_revenue DESC
+)
+SELECT DISTINCT name, total.total_reviews, play.install_count as play_store_installs, total.lifetime, app.content_rating, play.content_rating, app.primary_genre, play.genres, total.total_revenue
+FROM total_revenue AS total
+	FULL OUTER JOIN app_store_apps AS app
+		USING (name)
+	FULL OUTER JOIN play_store_apps AS play
+		USING (name)
+ORDER BY total.total_revenue DESC
+LIMIT 10;
